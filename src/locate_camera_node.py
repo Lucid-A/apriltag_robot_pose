d4e354852b6f526fb4d9d9e025e47481a2041578
board_2 <nx@board_2>
2023-03-02 22:01:05 +0800

    replace map to tag and add is_available topic

--- src/locate_camera_node.py
+++ src/locate_camera_node.py
@@ -2,16 +2,17 @@
 
 import rospy as ros
 from myTransformations import *
-from std_msgs.msg import Header
-from geometry_msgs.msg import PoseStamped, TransformStamped
-from apriltag_ros.msg import AprilTagDetection, AprilTagDetectionArray
+from std_msgs.msg import Bool
+from geometry_msgs.msg import PoseStamped
+from apriltag_ros.msg import AprilTagDetectionArray
 
 class LocateCameraNode:
     def __init__(self):
         self.rate = ros.Rate(10) # Hz
         self.sub = ros.Subscriber("/tag_detections", AprilTagDetectionArray, self.callback)
-        self.pub1 = ros.Publisher("/pose/map_in_cam", PoseStamped, queue_size=10)
-        self.pub2 = ros.Publisher("/pose/cam_in_map", PoseStamped, queue_size=10)
+        self.pub0 = ros.Publisher("/tag_detections/is_available", Bool, queue_size=10)
+        self.pub1 = ros.Publisher("/pose/tag_in_cam", PoseStamped, queue_size=10)
+        self.pub2 = ros.Publisher("/pose/cam_in_tag", PoseStamped, queue_size=10)
 
         ros.spin()
 
@@ -19,26 +20,28 @@ class LocateCameraNode:
         cnt = len(msg.detections)
         if not cnt:
             print("No tag detected!")
+            self.pub0.publish(Bool(False))
         elif 1 == cnt:
             #h = msg.header
             tag = msg.detections[0]
             #header = tag.pose.header
             
-            pose_map_in_cam = AprilTagDetection2PoseStamped(tag)
-            pose_cam_in_map = Transform2PoseStamped(
+            pose_tag_in_cam = AprilTagDetection2PoseStamped(tag)
+            pose_cam_in_tag = Transform2PoseStamped(
                                 inverseTransform(
-                                  PoseStamped2Transform(pose_map_in_cam, "map")
+                                  PoseStamped2Transform(pose_tag_in_cam, "tag")
                                 )
                               )
             try:
-                print("pose_map_in_cam:\n", pose_map_in_cam)
-                print("pose_cam_in_map:\n", pose_cam_in_map)
-                self.pub1.publish(pose_map_in_cam)
-                self.pub2.publish(pose_cam_in_map)
+                print("pose_tag_in_cam:\n", pose_tag_in_cam)
+                print("pose_cam_in_tag:\n", pose_cam_in_tag)
+                self.pub0.publish(Bool(True))
+                self.pub1.publish(pose_tag_in_cam)
+                self.pub2.publish(pose_cam_in_tag)
             except ros.ROSInterruptException:
                 pass
         else:
-            print(f"{len(msg.detections)} tags are detected!")
+            print(f"{cnt} tags are detected!")
                 
 if __name__ == "__main__":
     ros.init_node("locate_camera", anonymous=False)
